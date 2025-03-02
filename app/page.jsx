6d2dbdc3fc4bd8e52f495088754cf6c3222f44d7
Devansh <devanshjindal31@gmail.com>
2025-03-02 22:16:29 +0530

    heigh adjusted

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import SideBar from "@/components/SideBar/SideBar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col xl:flex-row bg-black px-3 md:px-20 lg:px-40 gap-10 py-5 md:py-16">
+    <div className="flex flex-col xl:flex-row bg-black px-3 md:px-20 lg:px-40 gap-10 py-5 md:py-16 min-h-screen">
       <SideBar />
       <div className="bg-green-500">main content scroll with full page</div>
     </div>
