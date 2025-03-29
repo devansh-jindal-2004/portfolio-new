5f8da31d7b925f5acda31e789991f4d2c3cd200c
Devansh <devanshjindal31@gmail.com>
2025-03-29 13:43:48 +0530

    starting about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='h-full overflow-hidden'>
-      <div className='text-2xl px-10 py-5'>
+    <div className='h-full w-full px-4 md:px-8 py-6 space-y-6'>
+      <div className="text-3xl font-bold text-gray-100 relative w-fit">
         About
+        <div className="h-1 w-1/2 bg-[#fce469] rounded-full mt-5 p-1"></div>
       </div>
       
     </div>
--- components/Portfolio/Portfolio.jsx
+++ components/Portfolio/Portfolio.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div className='h-full w-full px-4 md:px-8 py-6 space-y-6'>
+      <div className="text-3xl font-bold text-gray-100 relative w-fit">
+        Portfolio
+        <div className="h-1 w-1/2 bg-[#fce469] rounded-full mt-5 p-1"></div>
+      </div>
+
+      coming Soon
+      
+    </div>
   )
 }
 
