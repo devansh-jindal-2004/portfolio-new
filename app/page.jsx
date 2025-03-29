b0a63ebb777fe57bcef43e52d804caef3ae1728f
Devansh <devanshjindal31@gmail.com>
2025-03-29 12:51:18 +0530

    bottom navbar problem adjusted

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import SideBar from "@/components/SideBar/SideBar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col xl:flex-row bg-black px-3 sm:px-20 lg:px-40 gap-5 xl:gap-10 py-5 md:py-16 min-h-screen justify-center">
+    <div className="flex flex-col xl:flex-row bg-black px-3 sm:px-20 lg:px-40 gap-5 xl:gap-10 py-5 md:py-16 min-h-screen justify-center pb-10">
       <SideBar />
       <Body />
     </div>
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar({ setSelected, selected }) {
   const links = ["About", "Resume", "Portfolio", "Contact"];
 
   return (
-    <div className="fixed md:absolute right-0 bottom-0 xl:top-0 bg-[#232323f2]/95 shadow-lg flex xl:px-5 py-2 xl:py-3 rounded-t-2xl xl:rounded-t-none xl:rounded-se-3xl xl:rounded-es-3xl h-fit w-full xl:w-fit justify-center items-center z-50">
+    <div className="fixed xl:absolute right-0 bottom-0 xl:top-0 bg-[#232323f2]/95 shadow-lg flex xl:px-5 py-2 xl:py-3 rounded-t-2xl xl:rounded-t-none xl:rounded-se-3xl xl:rounded-es-3xl h-fit w-full xl:w-fit justify-center items-center z-50">
       <ul className="xl:space-y-0 xl:space-x-6 flex gap-5 py-2 px-10 pb-5 pt-3">
         {links.map((link, index) => (
           <li key={index}>
