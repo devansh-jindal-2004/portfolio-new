8b880bc1f0504a0382702a386be0e565b0a3ff8c
Devansh Jindal <devanshjindal31@gmail.com>
2026-06-16 09:27:15 +0530

    Refactor code structure for improved readability and maintainability

--- components/Portfolio/projects.js
+++ components/Portfolio/projects.js
@@ -14,6 +14,36 @@ const projects = [
           technologies: ["React", "Next.js", "MongoDB"],
           image: "/projects/MTTFHUB.png",
         },
+        {
+          title: "Dashmesh Scaled Models Makers",
+          description: "A fully functional website for a business Selling 3d printed models on custom orders and pre defined orders",
+          technologies: ["React", "Next.js", "MongoDB", "Express Js", "Nodemailer"],
+          image: "/projects/Dashmesh.png",
+        },
+        {
+          title: "Hostel Management System",
+          description: "A Complete software to manage staff and Students in a hostel",
+          technologies: ["React", "Next.js", "MongoDB", "Express Js", "Nodemailer", "Type Script", "Tailwind Css"],
+          image: "/projects/hostelManagement.png",
+        },
+        {
+          title: "Seating Plan Management System",
+          description: "A Complete software to manage Seating arrangement in examination and excel import for the Seating arrangement generated",
+          technologies: ["React", "Next.js", "MongoDB", "Express Js", "Nodemailer", "Type Script", "Tailwind Css"],
+          image: "/projects/SeatingPlanManagement.png",
+        },
+        {
+          title: "Narindra Techno Tools",
+          description: "An eCommerce site for a business to Showcase its products online and selling them online",
+          technologies: ["React", "Next.js", "MongoDB", "Nodemailer", "Type Script", "Tailwind Css"],
+          image: "/projects/narindraTechnoTools.png",
+        },
+        {
+          title: "Glossify",
+          description: "An eCommerce site for a Car care brand to start its business Online",
+          technologies: ["React", "Next.js", "MongoDB", "Nodemailer", "Type Script", "Tailwind Css"],
+          image: "/projects/Glossify.png",
+        },
       ],
     },
   ];
